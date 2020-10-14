14499875967f78ef3a45cfcfea549c2b4e43bc76
varuni-bit <varuni3443@gmail.com>
2020-10-14 19:52:06 +0530

    storing cart value on locatStrorage,in index.html added javascript code for the same

--- ecomsite/urls.py
+++ ecomsite/urls.py
@@ -21,4 +21,5 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('<int:id>/', views.detail, name='detail'),
 ]
